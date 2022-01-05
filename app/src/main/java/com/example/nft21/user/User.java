c0c13ca820e88174c6518a2c334f3129dd30fc50
Daryl CARUSO <daryl.caruso@etu.univ-nantes.fr>
2022-01-05 16:01:45 +0100

    feat: creations d'une classe User

---
+++ app/src/main/java/com/example/nft21/user/User.java
@@ -0,0 +1,13 @@
+package com.example.nft21.user;
+
+public class User {
+    private String username;
+    private String email;
+    private String password;
+
+    public User(String username,String email,String password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
