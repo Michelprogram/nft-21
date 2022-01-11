a00ee665005896772487805a88360e2f0523037a
Daryl CARUSO <daryl.caruso@etu.univ-nantes.fr>
2022-01-11 12:18:41 +0100

    feat: register page

--- app/src/main/java/com/example/nft21/HomeActivity.java
+++ app/src/main/java/com/example/nft21/HomeActivity.java
@@ -2,15 +2,39 @@ package com.example.nft21;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+    private final int LOGIN_REQUEST_CODE = 21;
+    private final int REGISTER_REQUEST_CODE = 12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent request = new Intent(HomeActivity.this,LoginActivity.class);
+                startActivityForResult(request,LOGIN_REQUEST_CODE);
+            }
+        });
+
+        Button registerButton = (Button) findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent request = new Intent(HomeActivity.this,RegisterActivity.class);
+                startActivityForResult(request,REGISTER_REQUEST_CODE);
+            }
+        });
+
     }
 }
