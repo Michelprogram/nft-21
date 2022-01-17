df252a11825196cfa89f77d78cb728d350123948
Daryl CARUSO <daryl.caruso@etu.univ-nantes.fr>
2022-01-17 11:39:15 +0100

    feat : iPanier

--- app/src/main/java/com/example/nft21/ShopActivity.java
+++ app/src/main/java/com/example/nft21/ShopActivity.java
@@ -4,11 +4,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ShopActivity extends AppCompatActivity {
+import com.example.nft21.nft.NFT;
+import com.example.nft21.user.User;
+import com.example.nft21.user.iPanier;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ShopActivity extends AppCompatActivity implements iPanier {
+    private Map<User, ArrayList<NFT>> panier = new HashMap<>();//panier
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+    }
+
+    @Override
+    public void ajouterAuPanier(User client, NFT article) {
+        panier.get(client).add(article);
+    }
+
+    @Override
+    public void supprimerDuPanier(User client, NFT article) {
+        panier.get(client).remove(article);
+    }
+
+    @Override
+    public double consulterMontantPanierETH(User client) {
+        return 0;
+    }
+
+    @Override
+    public double consulterMontantPanierEuros(User client) {
+        return 0;
+    }
+
+    @Override
+    public void viderPanier(User client) {
+
+    }
+
+    @Override
+    public List<NFT> listerCommandes(User client) {
+        return null;
     }
 }
