5c64c0c86a9391da1be91e43f8202a5db997dd94
Daryl CARUSO <daryl.caruso@etu.univ-nantes.fr>
2022-01-18 10:08:59 +0100

    feat : sending nfts to cart

--- app/src/main/java/com/example/nft21/CartActivity.java
+++ app/src/main/java/com/example/nft21/CartActivity.java
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.example.nft21.NFT.NFT;
+import com.example.nft21.user.User;
+
+import java.util.ArrayList;
+
 public class CartActivity extends AppCompatActivity {
 
     @Override
@@ -14,14 +19,35 @@ public class CartActivity extends AppCompatActivity {
 
         //récupération des infos des achats
         Bundle extras = getIntent().getExtras();
-        double totalETH = extras.getDouble("totalETH");
-        double totalEuros = extras.getDouble("totalEuros");
+        ArrayList<NFT> nfts = extras.getParcelableArrayList("panier");
+
+        //calcul du montant total
+        double totalETH = this.consulterMontantPanierETH(nfts);
+        double totalEuros = this.consulterMontantPanierEuros(nfts);
 
         //mise à jour de la vue
         TextView totalETHText = (TextView) findViewById(R.id.cartTotalPriceETH);
         TextView totalEurosText = (TextView) findViewById(R.id.cartTotalPriceEuros);
 
         totalETHText.setText(Double.toString(totalETH));
-        totalETHText.setText(Double.toString(totalEuros));
+        totalEurosText.setText(Double.toString(totalEuros));
+    }
+
+    public double consulterMontantPanierETH(ArrayList<NFT> nfts) {
+        double montant = 0.0;
+        for(NFT nft : nfts){
+            montant+=nft.getPrice();
+        }
+        return montant;
+    }
+
+    public double consulterMontantPanierEuros(ArrayList<NFT> nfts) {
+        double montant = 0.0;
+        for(NFT nft : nfts){
+            montant+=nft.getPrice();
+        }
+
+        //convertir en euros !!!
+        return montant;
     }
 }
--- app/src/main/java/com/example/nft21/ShopActivity.java
+++ app/src/main/java/com/example/nft21/ShopActivity.java
@@ -9,14 +9,13 @@ import android.os.Bundle;
 
 import com.example.nft21.NFT.NFT;
 import com.example.nft21.user.User;
-import com.example.nft21.user.iPanier;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class ShopActivity extends AppCompatActivity implements iPanier {
+public class ShopActivity extends AppCompatActivity {
     private Map<User, ArrayList<NFT>> panier = new HashMap<>();//panier
 
     @Override
@@ -32,54 +31,26 @@ public class ShopActivity extends AppCompatActivity implements iPanier {
         nfts.add(new NFT("nft trop nul","img","un nft que emma adore",0.1,0));
         panier.put(daryl,nfts);
 
-        //calcul du montant et transfer à l'activité panier
-        double totalETH = this.consulterMontantPanierETH(daryl);
-        double totalEuros = this.consulterMontantPanierEuros(daryl);
-
+        //tranfer au panier
         Intent intent = new Intent(ShopActivity.this,CartActivity.class);
-        intent.putExtra("totalETH",totalETH);
-        intent.putExtra("totalEuros",totalEuros);
+        intent.putExtra("panier",nfts);
         startActivity(intent);
     }
 
     //------------------------------------gestion du panier--------------------------------
-    @Override
     public void ajouterAuPanier(User client, NFT article) {
         panier.get(client).add(article);
     }
 
-    @Override
     public void supprimerDuPanier(User client, NFT article) {
         panier.get(client).remove(article);
     }
 
-    @Override
-    public double consulterMontantPanierETH(User client) {
-        double montant = 0.0;
-        for(NFT nft : panier.get(client)){
-            montant+=nft.getPrice();
-        }
-        return montant;
-    }
-
-    @Override
-    public double consulterMontantPanierEuros(User client) {
-        double montant = 0.0;
-        for(NFT nft : panier.get(client)){
-            montant+=nft.getPrice();
-        }
-
-        //convertir en euros !!!
-        return montant;
-    }
-
     @RequiresApi(api = Build.VERSION_CODES.N)
-    @Override
     public void viderPanier(User client) {
         panier.replace(client,new ArrayList<NFT>());
     }
 
-    @Override
     public List<NFT> listerCommandes(User client) {
         return panier.get(client);
     }
