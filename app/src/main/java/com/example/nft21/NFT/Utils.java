2ae1270cdd03983a040c2946c264b84b9571b37a
Daryl CARUSO <daryl.caruso@etu.univ-nantes.fr>
2022-01-19 10:41:52 +0100

    feat : debut de merge ok

---
+++ app/src/main/java/com/example/nft21/NFT/Utils.java
@@ -0,0 +1,32 @@
+package com.example.nft21.NFT;
+
+import java.util.Random;
+
+public class Utils {
+
+    public static Double randomEthPrice(){
+        Double MAX = 3.0, MIN = 1.10;
+        Double result = (Math.random() * (MAX - MIN) + 1) + MIN;
+
+        String tempo = String.format("%.2f",result);
+
+        return Double.parseDouble(tempo);
+    }
+
+    public static Integer mostViewed(){
+        Integer MAX = 5;
+        Integer result = (int)(Math.random() * MAX);
+
+        return (result == MAX - 1) ? 1 : 0;
+    }
+
+    public static Double convertEthToEuro(){
+        return 0.0;
+    }
+
+    public static Double convertEuroToEth(){
+        return 0.0;
+    }
+
+}
+
